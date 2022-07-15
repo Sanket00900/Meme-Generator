659780dc549edf0df2057f4ddfa454232616456c
Sanket_Jagdale <sanketsj00900@gmail.com>
2022-07-15 14:31:15 +0530

    add styles

--- src/Components/Memes.js
+++ src/Components/Memes.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Meme() {
+  const image = "./Images/meme-image.jpeg";
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg",
+    randomImage: image,
   });
   const [allMemes, setAllMemes] = React.useState([]);
 
